ec6a4cf2a69457153edee3ae3e65258e2cdda5f2
Grzegorz Unijewski <grzegorz@unijewski.pl>
2017-10-18 20:14:22 +0200

    Create entries table

---
+++ db/migrate/20171016214012_create_entries.rb
@@ -0,0 +1,10 @@
+class CreateEntries < ActiveRecord::Migration[5.1]
+  def change
+    create_table :entries do |t|
+      t.string :user_uuid, null: false
+      t.string :full_name, null: false
+      t.string :avatar_url
+      t.timestamps null: false
+    end
+  end
+end
