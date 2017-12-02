0eab51ff054cdec5be602f039ba8a58e60188479
Grzegorz Unijewski <grzegorz@unijewski.pl>
2017-12-02 17:41:46 +0100

    Add counter cache to users table

---
+++ db/migrate/20171202163843_add_counter_cache_to_users.rb
@@ -0,0 +1,5 @@
+class AddCounterCacheToUsers < ActiveRecord::Migration[5.1]
+  def change
+     add_column :users, :entries_count, :integer, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018193457) do
+ActiveRecord::Schema.define(version: 20171202163843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20171018193457) do
     t.string "avatar_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "entries_count", default: 0
     t.index ["user_uuid"], name: "index_users_on_user_uuid"
   end
 
