38fb6b7519aa49c9bc828a98d59f125b534a997d
Grzegorz Unijewski <grzegorz@unijewski.pl>
2017-12-03 19:14:29 +0100

    Validate presence of body attr in Entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,7 @@
 class Entry < ActiveRecord::Base
   belongs_to :user, counter_cache: true
 
+  validates :body, presence: true
+
   scope :latest, -> { includes(:user).order(created_at: :desc) }
 end
