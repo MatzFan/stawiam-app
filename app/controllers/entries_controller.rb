4bcf3cda2c62f2fdcc1840f0ba735e645ba28917
Grzegorz Unijewski <grzegorz@unijewski.pl>
2017-12-03 19:05:17 +0100

    Paginate entries and set proper view

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,9 @@
 class App
+  get "/entries" do
+    @entries = Entry.latest.paginate(page: params[:page])
+    haml :"entries/index"
+  end
+
   post "/entries" do
     content_type :json
     EntryWorker.perform_async(params)
