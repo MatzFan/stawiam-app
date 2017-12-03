fa76c86d3e6781fb3ccfbf6b6fa4aa22c04ee625
Grzegorz Unijewski <grzegorz@unijewski.pl>
2017-12-03 22:11:01 +0100

    Define sprockets properly

--- app.rb
+++ app.rb
@@ -8,26 +8,26 @@ class App < Sinatra::Base
   register WillPaginate::Sinatra
 
   set :app_file, __FILE__
-  set :environment, Sprockets::Environment.new
   set :haml, format: :html5, layout: :application
   set :root, File.dirname(__FILE__)
+  set :sprockets, Sprockets::Environment.new
   set :views, File.join(root, "app/views")
 
   # Load all initializers
   require File.join(root, "/config/initializers/auto_loader.rb")
 
   # Assets
-  environment.append_path "app/assets/stylesheets"
-  environment.append_path "app/assets/javascripts"
+  sprockets.append_path "app/assets/stylesheets"
+  sprockets.append_path "app/assets/javascripts"
   # Workaround https://github.com/glebm/popper_js-rubygem/issues/5
-  environment.append_path(
+  sprockets.append_path(
     File.join(Gem::Specification.find_by_name("popper_js").gem_dir, "assets", "javascripts"),
   )
-  environment.js_compressor  = Uglifier.new(harmony: true)
-  environment.css_compressor = :scss
+  sprockets.js_compressor  = Uglifier.new(harmony: true)
+  sprockets.css_compressor = :scss
   get "/app/assets/*" do
     env["PATH_INFO"].sub!("/app/assets", "")
-    settings.environment.call(env)
+    settings.sprockets.call(env)
   end
 
   # Root path
