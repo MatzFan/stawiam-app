69f28f1c965665b79e87c76afa2902173c37ced9
Grzegorz Unijewski <grzegorz@unijewski.pl>
2017-12-03 18:00:13 +0100

    Remove redundant space

--- db/migrate/20171202163843_add_counter_cache_to_users.rb
+++ db/migrate/20171202163843_add_counter_cache_to_users.rb
@@ -1,5 +1,5 @@
 class AddCounterCacheToUsers < ActiveRecord::Migration[5.1]
   def change
-     add_column :users, :entries_count, :integer, default: 0
+    add_column :users, :entries_count, :integer, default: 0
   end
 end
