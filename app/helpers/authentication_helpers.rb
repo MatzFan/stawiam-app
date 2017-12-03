3aeb5875d878f0cef5811990c5142fef9892fdae
Grzegorz Unijewski <grzegorz@unijewski.pl>
2017-12-03 22:29:44 +0100

    Create authentication helpers

---
+++ app/helpers/authentication_helpers.rb
@@ -0,0 +1,15 @@
+class App
+  helpers do
+    def protected!
+      return if authorized?
+      headers["WWW-Authenticate"] = "Basic realm=\"Restricted Area\""
+      halt 401, "Not authorized\n"
+    end
+
+    def authorized?
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? && @auth.basic? && @auth.credentials &&
+        @auth.credentials == [AppConfig.authentication.username, AppConfig.authentication.password]
+    end
+  end
+end
