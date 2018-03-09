cf59eab71d83a4b76a548670206d43861b7cbb72
Grzegorz Unijewski <grzegorz@unijewski.pl>
2018-03-09 09:42:25 +0100

    Add missing presence validation of user association in Entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,7 +1,7 @@
 class Entry < ActiveRecord::Base
   belongs_to :user, counter_cache: true
 
-  validates :body, presence: true
+  validates :body, :user, presence: true
 
   scope :latest, -> { includes(:user).order(created_at: :desc) }
 end
